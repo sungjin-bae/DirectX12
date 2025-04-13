200a44fcf98dfd058ee2dadb6b2ee9214b8c0958
baehun92 <baehun92@naver.com>
2025-04-13 20:48:53 +0900

    Renderer 코딩 스타일 맞지 않는 코드들 수정.

--- MyDirect/MyDirect/Source/Renderer/Renderer.h
+++ MyDirect/MyDirect/Source/Renderer/Renderer.h
@@ -20,32 +20,32 @@ private:
     void CreateSwapChain();
 
     void LogAdapters();
-    void LogAdapterOutputs(IDXGIAdapter* adapter);
-    void LogOutputDisplayModes(IDXGIOutput* output, DXGI_FORMAT format);
+    void LogAdapterOutputs(IDXGIAdapter* in_adapter);
+    void LogOutputDisplayModes(IDXGIOutput* in_output, DXGI_FORMAT in_format);
 
-    bool      m4xMsaaState = false;    // 4X MSAA enabled
+    bool      m_4x_msaa_state = false;    // 4X MSAA enabled
 
-    UINT      m4xMsaaQuality = 0;      // quality level of 4X MSAA
+    UINT      m_4x_msaa_quality = 0;      // quality level of 4X MSAA
 
-    Microsoft::WRL::ComPtr<IDXGIFactory4> mdxgiFactory;
-    Microsoft::WRL::ComPtr<IDXGISwapChain> mSwapChain;
-    Microsoft::WRL::ComPtr<ID3D12Device> md3dDevice;
+    Microsoft::WRL::ComPtr<IDXGIFactory4> m_dxgi_factory;
+    Microsoft::WRL::ComPtr<IDXGISwapChain> m_swap_chain;
+    Microsoft::WRL::ComPtr<ID3D12Device> m_d3d_device;
 
-    Microsoft::WRL::ComPtr<ID3D12Fence> mFence;
-    UINT64 mCurrentFence = 0;
+    Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
+    UINT64 m_current_fence = 0;
 
-    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> mDirectCmdListAlloc;
+    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_direct_cmd_list_alloc;
 
     static const int SwapChainBufferCount = 2;
 
-    UINT mRtvDescriptorSize = 0;
-    UINT mDsvDescriptorSize = 0;
-    UINT mCbvSrvUavDescriptorSize = 0;
+    UINT m_rtv_descriptor_size = 0;
+    UINT m_dsv_descriptor_size = 0;
+    UINT m_cbv_srv_uav_descriptor_size = 0;
 
-    DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+    DXGI_FORMAT m_back_buffer_format = DXGI_FORMAT_R8G8B8A8_UNORM;
 
-    int mClientWidth = 800;
-    int mClientHeight = 600;
+    int m_client_width = 800;
+    int m_client_height = 600;
 };
 
 
